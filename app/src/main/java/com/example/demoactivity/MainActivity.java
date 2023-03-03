404e39bc2d1c738279ef9b4b492349c6a2f8a35b
shetingting <408405378@qq.com>
2023-03-03 17:01:27 +0800

    first test

--- app/src/main/java/com/example/demoactivity/MainActivity.java
+++ app/src/main/java/com/example/demoactivity/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -66,5 +67,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+        Log.d("Main","test commit");
     }
 }
