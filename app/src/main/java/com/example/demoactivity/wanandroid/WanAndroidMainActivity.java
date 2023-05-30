d709f4c697c56cbe5e4c9cc55ef26f01ea715c69
shetingting <408405378@qq.com>
2023-05-30 14:53:53 +0800

    list下拉刷新

--- app/src/main/java/com/example/demoactivity/wanandroid/WanAndroidMainActivity.java
+++ app/src/main/java/com/example/demoactivity/wanandroid/WanAndroidMainActivity.java
@@ -5,6 +5,7 @@ import android.widget.Toast;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.demoactivity.R;
 import com.example.demoactivity.netWork.HttpCallback;
@@ -23,18 +24,51 @@ public class WanAndroidMainActivity extends BaseActivity {
 
     private MainRepository mainRepository;
 
+    private SwipeRefreshLayout swipeLayout;
     private RecyclerView articleListView;
     private MainArticleAdapter articleAdapter;
 
     @Override
     public void initView() {
+        swipeLayout = findViewById(R.id.swipe_layout);
         articleListView = findViewById(R.id.rv_article);
-
     }
 
     @Override
     public void initData() {
         mainRepository = new MainRepository();
+
+        //设置下拉刷新loading圆圈的颜色
+        swipeLayout.setColorSchemeColors(getResources().getColor(R.color.colorAccent));
+        //设置下拉刷新loading背景颜色
+        swipeLayout.setProgressBackgroundColorSchemeColor(getResources().getColor(R.color.color_dark_gray));
+        //设置刷新监听事件
+        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mainRepository.getMainArticleList(0, new HttpCallback() {
+                    @Override
+                    public void onSucceed(BaseResponse response) {
+                        ArticleListBean articleListBean = JSONUtil.parseToArticleListBean(response.getData());
+                        if (articleListBean == null) {
+                            onFailed(0, null);
+                            return;
+                        }
+                        List<ArticleBean> articleList = articleListBean.getDatas();
+                        List<ArticleBean> list = removeNullTags(articleList);
+                        articleAdapter.setArticleList(articleList);
+                        articleAdapter.notifyDataSetChanged();
+                        swipeLayout.setRefreshing(false);
+                    }
+
+                    @Override
+                    public void onFailed(int code, String message) {
+                        swipeLayout.setRefreshing(false);
+                        Toast.makeText(WanAndroidMainActivity.this, TextUtils.isEmpty(message) ? "文章内容为空！" : message, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
 
         articleAdapter = new MainArticleAdapter(this);
         articleListView.setLayoutManager(new LinearLayoutManager(this));
